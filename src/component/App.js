00ad2c7a2897369d885dd2709f2b836c22486f97
devwoodie <ehddl453@naver.com>
2022-09-17 00:55:27 +0900

    feat : portfolio website setting

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import '../css/reset.css';
+import '../css/App.scss';
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1 className="a">WOODIE YU DONG WOO</h1>
+            <h1 className="b">WOODIE YU DONG WOO</h1>
+        </div>
+    );
+}
+
+export default App;
