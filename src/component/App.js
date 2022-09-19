445ba459a91d3e26c2414f5b38fa13b2e7881176
devwoodie <ehddl453@naver.com>
2022-09-19 23:41:52 +0900

    feat : mode, nav-tab ui setting

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,44 @@
 import '../css/reset.css';
 import '../css/App.scss';
-
+import { BsFillSunFill, BsFillMoonFill} from 'react-icons/bs'
+import {useCallback, useState} from "react";
 
 const App = () => {
+
+    let [mode, setMode] = useState(false);
+
+    //모드 변경 false=dark / true=light
+    const modeChange = useCallback(() => {
+        if(mode === false){
+            return setMode(true);
+        }else{
+            return setMode(false);
+        };
+    },[mode]);
+
     return (
-        <div className="App">
-            <h1 className="a">WOODIE YU DONG WOO</h1>
-            <h1 className="b">WOODIE YU DONG WOO</h1>
+        <div className="App inner">
+            <div className="mode" onClick={modeChange}>
+                { mode === true ?
+                    <span>
+                        <BsFillMoonFill className="mode-icon" />
+                        DARK MODE
+                    </span>:
+                    <span>
+                        <BsFillSunFill className="mode-icon" />
+                        LIGHT MODE
+                    </span> }
+            </div>
+            <div className="content">
+                <h1 className="woodie">WOODIE</h1>
+                <h2 className="my-name">YU<br/>DONG WOO</h2>
+            </div>
+            <ul className="nav-tab">
+                <li className="tab active">INTRO</li>
+                <li className="tab">ABOUT</li>
+                <li className="tab">PROJECTS</li>
+                <li className="tab">CONTACT</li>
+            </ul>
         </div>
     );
 }
