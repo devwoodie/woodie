049580bfbb9968fc407fed5743cf81d6c6a467af
devwoodie <ehddl453@naver.com>
2022-09-24 21:38:42 +0900

    feat : career json data add

--- src/component/About.js
+++ src/component/About.js
@@ -3,6 +3,7 @@ import '../css/About.scss';
 const About = ({ infoJson }) => {
 
     let infoData = infoJson.info;
+    let careerData = infoJson.career
 
     return(
         <div className="About">
@@ -13,7 +14,19 @@ const About = ({ infoJson }) => {
                 {infoData.map((info, index) => (
                     <li className="info-list" key={index}>
                         <span className="info-tit">{info.title}</span>
-                        <span className="info-cont">{info.cont}</span>
+                        <span className="info-cont">{info.content}</span>
+                    </li>
+                ))}
+            </ul>
+            <ul className="career">
+                <li className="career-list">
+                    <span className="career-title">Careers</span>
+                </li>
+                {careerData.map((career, index) => (
+                    <li className="career-list" key={index}>
+                        <span className="career-tit">{career.title}</span>
+                        <span className="career-sub">{career.sub}</span>
+                        <span className="career-date">{career.date}</span>
                     </li>
                 ))}
             </ul>
