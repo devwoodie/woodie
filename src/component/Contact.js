58d3298b97866a78f8de5f7450280b2fc7b65989
devwoodie <ehddl453@naver.com>
2022-09-29 22:21:11 +0900

    feat : contact page sketch

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,8 +2,25 @@ import '../css/Contact.scss';
 
 const Contact = () => {
     return(
-        <div>
-
+        <div className="Contact">
+            <div className="top-text">
+                <p className="hello">
+                    Hello, my name is Yu Dongwoo.<br/>
+                    I'm a junior developer who enjoys learning, growing and change to suit.
+                </p>
+                <p className="will">
+                    I'll continue to grow my front-end skills.
+                </p>
+                <p className="bold-text">
+                    " Enjoy learning and growing "<br/>
+                    " Able to adapt to change "
+                </p>
+                <p className="want">
+                    I would like to continue to challenge myself and grow in a wider place,<br/>
+                    Show you how to change according to the trend rather than staying in one place.
+                </p>
+            </div>
+            <h2 className="contact-tit">CONTACT<span className="me">ME</span></h2>
         </div>
     )
 }
