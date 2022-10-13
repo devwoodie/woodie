32435ba996bdb44c3eee9ffcd77a433a00843f8c
devwoodie <ehddl453@naver.com>
2022-10-14 01:03:48 +0900

    feat : project component data rendering

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -1,10 +1,25 @@
 import '../css/Projects.scss';
 
 const Projects = ({ projectJson }) => {
-    console.log(projectJson)
+
+    let projectData = projectJson.project;
+
     return(
         <div className="Project">
-
+            <div className="contents-head">
+                <span className="contents-title projects">Projects</span>
+            </div>
+            <ul className="contents">
+                {projectData.map((project, index) => (
+                    <li className="contents-list" key={index}>
+                        <a target="_blank" href={'https://github.com/devwoodie/'+project.git}>
+                            <span className="contents-tit">{project.title}</span>
+                            <span className="contents-sub">{project.sub}</span>
+                            <span className="contents-date">{project.type}</span>
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 };
